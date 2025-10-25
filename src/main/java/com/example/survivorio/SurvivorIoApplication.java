21f7cfc08fbced0b77daa1ffb095dc707b398e03
Muhamet-Can Oez <moez@ceteris.ag>
2025-10-25 10:47:15 +0200

    1.MS

---
+++ src/main/java/com/example/survivorio/SurvivorIoApplication.java
@@ -0,0 +1,11 @@
+package com.example.survivorio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SurvivorIoApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SurvivorIoApplication.class, args);
+    }
+}
---
+++ src/main/java/com/example/survivorio/controller/PlayerController.java
@@ -0,0 +1,14 @@
+package com.example.survivorio.controller;
+
+import com.example.survivorio.entity.Player;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class PlayerController {
+
+    @GetMapping("/player")
+    public Player getPlayer() {
+        return new Player(1L, "Hero", 5);
+    }
+}
